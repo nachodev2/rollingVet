55e9e76fd103904898c0f447d4c31ee8c2778464
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-09-28 13:42:24 -0300

    Modifiqué los colores del título y la imagen de la página de error404

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import Error404 from "./components/pages/error404/Error404";
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import './index.css'
 
 function App() {
   return (
     <>
+      <Error404 />
     </>
   );
 }
