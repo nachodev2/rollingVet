3d220bb534ee74fd49f6432d87d6a702e65cb806
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-09-29 20:41:08 -0300

    Agregué el título y el contenedor de los datos del paciente

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import Paciente from "./components/pages/paciente/Paciente";
 
 function App() {
   return (
     <>
-    <h1 className="text-center my-3 titulo">Historia Clínica</h1>
       <Paciente></Paciente>
-      
     </>
   );
 }
--- src/components/pages/paciente/Paciente.jsx
+++ src/components/pages/paciente/Paciente.jsx
@@ -2,8 +2,9 @@ function Paciente() {
 
   return (
     <>
-    <div className="container mt-4 border border-info">
-        <h3>Datos del Paciente</h3>
+    <h1 className="text-center my-3 titulo text-info">Historia Clínica</h1>
+    <div className="container mt-4 border border-dark rounded-3 p-3 bg-body-secondary">
+        <h4 className="text-center">Datos del Paciente</h4>
     </div>
     </>
   )
