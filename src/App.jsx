3f4a1e3b4b64987fc4d912960af3ef1cc97a1d72
Jose Romero <kira1698@gmail.com>
2025-09-29 12:50:50 -0300

    se agrega la estrutura del boton modal para ingresar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Error404 from "./components/pages/error404/Error404";
+import Login from "./components/pages/loginRegistro/Login";
 import './index.css'
 
 function App() {
   return (
     <>
+      <Login />
       <Error404 />
+
     </>
   );
 }
