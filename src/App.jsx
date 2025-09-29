143d1f01fdaf671cc1bfe7d3382adc89c00a1cb3
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-09-29 20:33:00 -0300

    Creé el componente en el que está la ficha de paciente

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Paciente from "./components/pages/paciente/Paciente";
 
 function App() {
-
   return (
     <>
+    <h1 className="text-center my-3 titulo">Historia Clínica</h1>
+      <Paciente></Paciente>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/pages/paciente/Paciente.jsx
@@ -0,0 +1,12 @@
+function Paciente() {
+
+  return (
+    <>
+    <div className="container mt-4 border border-info">
+        <h3>Datos del Paciente</h3>
+    </div>
+    </>
+  )
+}
+
+export default Paciente
