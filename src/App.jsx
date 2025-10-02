72e0e27eea5685da7c3787c8cdae1f42511efac8
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-10-02 12:48:26 -0300

    Agregué los import en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Paciente from "./components/pages/paciente/Paciente.jsx";
+import Menu from "./components/shared/Menu.jsx";
+import Error404 from "./components/pages/error404/Error404.jsx";
+import Footer from "./components/shared/Footer.jsx";
+import SobreNosotros from "./components/pages/sobre-nosotros/SobreNosotros";
 import Producto from "../src/components/pages/producto/Producto";
+import "./index.css";
 
 function App() {
   return (
