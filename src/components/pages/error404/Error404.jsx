8429ea6ed3750614a99e7f22cd287152a62f8900
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-10-02 12:27:39 -0300

    Importé el css en la página de error404

--- src/components/pages/error404/Error404.jsx
+++ src/components/pages/error404/Error404.jsx
@@ -1,9 +1,11 @@
+import "./error404.css";
+
 function Error404() {
   return (
     <>
       <div className="d-flex flex-column align-items-center">
         <h1 className="text-center mt-4 mx-1 text-primary">UPS! Esta página se perdió como nosotros</h1>
-        <img src="../images/error404.png" alt="Error404" className="w-50 img-error404"/>
+        <img src="../images/error404.png" alt="Error404" className="img-error404"/>
       </div>
     </>
   );
