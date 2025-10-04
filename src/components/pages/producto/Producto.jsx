6d54cf81aded1db35785748af262dcd9028221e5
Tomas Ruiz <ruiztms20@gmail.com>
2025-10-04 15:00:02 -0300

    Solucione errores de relacion de aspecto en imagenes de producto

--- src/components/pages/producto/Producto.jsx
+++ src/components/pages/producto/Producto.jsx
@@ -1,32 +1,35 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { CardImg } from "react-bootstrap";
 import "./producto.css";
 
-
 function Producto({ data }) {
-
   return (
-
-    <Card className="product-card mt-4 rounded-4 h-100">
-
+    <Card className="product-card mt-4 rounded-4">
+      
       <Card.Img
         variant="top"
         src={data.imagen}
         alt={data.alt}
-        className="img-card-productos d-flex m-auto h-100 contain"
+        className="product-card-img w-100"
         draggable={false}
       />
-      <Card.Body>
-        <Card.Title className="text-dark titulo fs-4">
-          {data.nombre}
-        </Card.Title>
-        <Card.Text className="texto-cuerpo">
-          {data.texto}
-        </Card.Text>
-        <Card.Text className="product-card-price">
-          {data.precio}
-        </Card.Text>
-        <Button variant="primary" className="w-100">Comprar</Button>
+      <Card.Body className="d-flex flex-column justify-content-between p-3"> 
+        <div className='mb-3'>
+            <Card.Title className="text-dark titulo fs-5 mb-1 text-truncate"> 
+              {data.nombre}
+            </Card.Title>
+            <Card.Text className="texto-cuerpo small text-muted mb-2 text-truncate"> 
+              {data.texto} 
+            </Card.Text>
+        </div>
+
+        <div>
+            <Card.Text className="product-card-price fs-4 fw-bold mb-2">
+              {data.precio}
+            </Card.Text>
+            <Button variant="success" className="w-100">Comprar</Button> 
+        </div>
       </Card.Body>
     </Card>
   );
